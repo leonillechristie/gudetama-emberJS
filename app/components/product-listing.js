7fbee1c05115f6eb2675ab22f6012f00a7938866
Leonille Christie <leonillechristie@gmail.com>
2018-09-24 16:34:08 +0800

    added delete in product

--- app/components/product-listing.js
+++ app/components/product-listing.js
@@ -5,6 +5,18 @@ export default Component.extend({
   actions: {
     toggleImageSize() {
       this.toggleProperty('isWide');
+    },
+    destroy(param) {
+	  var id = this.attrs.product.id;
+      $.ajax({
+        url: 'http://gudetama.local:8000/api/v1/products/' + id,
+          type: 'DELETE',
+          contentType: "application/x-www-form-urlencoded",
+          success: function(data) {
+            alert('delete successful');
+            window.location.replace("/products/");
+          }
+      });
     }
   }
 });
