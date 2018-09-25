cec05a179a3d1563875d47e3ada40ce3da057bc7
Leonille Christie <leonillechristie@gmail.com>
2018-09-25 13:48:27 +0800

    updated test cases

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -3,8 +3,6 @@ import { setupTest } from 'ember-qunit';
 
 module('Unit | Adapter | application', function(hooks) {
   setupTest(hooks);
-
-  // Replace this with your real tests.
   test('it exists', function(assert) {
     let adapter = this.owner.lookup('adapter:application');
     assert.ok(adapter);
--- tests/unit/controllers/products/index-test.js
+++ tests/unit/controllers/products/index-test.js
@@ -4,7 +4,6 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Controller | products/index', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function(assert) {
     let controller = this.owner.lookup('controller:products/index');
     assert.ok(controller);
