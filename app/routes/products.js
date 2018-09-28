9bfcd3db1644743720a09da9cde5c3fbceb34d59
Leonille Christie <leonillechristie@gmail.com>
2018-09-28 11:48:22 +0800

    updated routes for prod and users

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,7 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
-    return this.store.findAll('product');
-  }
+	currentSession: Ember.inject.service(),
+  user: null,
+  beforeModel: function(transition){
+      var vm = this;
+      return this.get('currentSession').loadCurrentUser().then((result) => {
+          if (transition.targetName !== 'login') {
+            var session = this.get('currentSession');
+            vm.set('user', session.user);
+
+            if (!session.isLoggedIn) {
+              this.transitionTo('login');
+            }
+          }
+      })
+  },
+	model() {
+		return this.store.findAll('product');
+	}
 });
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	beforeModel() {
+		var accessToken = localStorage.getItem('access_token');
+		var refreshToken = localStorage.getItem('refresh_token');
+
+		if (accessToken) {
+			$.ajax({
+			    url: 'http://gudetama.local:8000/api/v1/me',
+			    headers: {
+			    	"Authorization": "Bearer " + accessToken
+			    },
+			    success: function(response) {
+			    	
+			    }
+			});
+		} else {
+
+		}
+	},
   model() {
     return this.store.findAll('user');
   }
