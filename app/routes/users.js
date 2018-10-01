4f30930b6bf08bc69a1bcbb2f82ee60cf8f36aec
Leonille Christie <leonillechristie@gmail.com>
2018-10-01 12:05:05 +0800

    added sessions to users page

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,24 +1,39 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-	beforeModel() {
-		var accessToken = localStorage.getItem('access_token');
-		var refreshToken = localStorage.getItem('refresh_token');
+	currentSession: Ember.inject.service(),
+  	user: null,
+  	beforeModel: function(transition){
+      	var vm = this;
+      	return this.get('currentSession').loadCurrentUser().then((result) => {
+          	if (transition.targetName !== 'login') {
+            	var session = this.get('currentSession');
+            	vm.set('user', session.user);
 
-		if (accessToken) {
-			$.ajax({
-			    url: 'http://gudetama.local:8000/api/v1/me',
-			    headers: {
-			    	"Authorization": "Bearer " + accessToken
-			    },
-			    success: function(response) {
+	            if (!session.isLoggedIn) {
+	              this.transitionTo('login');
+	            }
+          	}
+      	})
+  	},
+	// beforeModel() {
+	// 	var accessToken = localStorage.getItem('access_token');
+	// 	var refreshToken = localStorage.getItem('refresh_token');
+
+	// 	if (accessToken) {
+	// 		$.ajax({
+	// 		    url: 'http://gudetama.local:8000/api/v1/me',
+	// 		    headers: {
+	// 		    	"Authorization": "Bearer " + accessToken
+	// 		    },
+	// 		    success: function(response) {
 			    	
-			    }
-			});
-		} else {
+	// 		    }
+	// 		});
+	// 	} else {
 
-		}
-	},
+	// 	}
+	// },
   model() {
     return this.store.findAll('user');
   }
