fef4b780036098fd24118c28f576999b3d4ed621
Leonille Christie <leonillechristie@gmail.com>
2018-10-01 10:30:27 +0800

    updated session service

--- app/services/current-session.js
+++ app/services/current-session.js
@@ -1,77 +1,77 @@
 import Ember from 'ember';
 // import Application from '@ember/application';
 
-// export default Ember.Service.extend({
+export default Ember.Service.extend({
 
-//     store: Ember.inject.service(),
-//     user: null,
-//     isLoggedIn: false,
-//     init() {
-//       this._super(...arguments);
-//       // app.deferReadiness();
-//       this.loadCurrentUser(); //if you are not calling loadCurrentUser method then this will ensure username and isLoggedIn set properly
-//     },
+    store: Ember.inject.service(),
+    user: null,
+    isLoggedIn: false,
+    init() {
+      this._super(...arguments);
+      // app.deferReadiness();
+      this.loadCurrentUser(); //if you are not calling loadCurrentUser method then this will ensure username and isLoggedIn set properly
+    },
 
-export function initialize(container, application) {
-  application.deferReadiness();
-  var session = container.lookup('service:session');
-  if (session.hasSession()) {
-    var store = Ember.inject.service()
-    var accessToken = localStorage.getItem('access_token');
-    var refreshToken = localStorage.getItem('refresh_token');
-    var vm = this;
-    if (!!accessToken && accessToken.length > 0) {
-    $.ajax({
-      url: 'http://gudetama.local:8000/api/v1/me',
-      headers: {
-        "Authorization": "Bearer " + accessToken
-      },
-      success: function(response) {
-          vm.set('user', response.data);
-          vm.set('isLoggedIn', true);
-          resolve();
-      },
-      error: function() {
-        $.ajax({
-          url: 'http://gudetama.local:8000/oauth/token',
-            method: 'POST',
-            data: {
-              grant_type: 'refresh_token',
-              client_id: 2,
-              client_secret: 'iMupCxM9eatJ963MUOoBp9uf5NAPfS3uZzebmp1I',
-              refresh_token: refreshToken,
-              scope: '*'
-            },
-            contentType: "application/x-www-form-urlencoded",
-            headers: {
-              'Accept': 'application/json'
-            },
-            success: function(response) {
-              localStorage.setItem('access_token', response.access_token);
-              localStorage.setItem('refresh_token', response.refresh_token);
-              // application.advanceReadiness();
-              resolve();
-            },
-            error: function() {
-              reject();
-            }
-        });
+// export function initialize(container, application) {
+//   application.deferReadiness();
+//   var session = container.lookup('service:session');
+//   if (session.hasSession()) {
+//     var store = Ember.inject.service()
+//     var accessToken = localStorage.getItem('access_token');
+//     var refreshToken = localStorage.getItem('refresh_token');
+//     var vm = this;
+//     if (!!accessToken && accessToken.length > 0) {
+//     $.ajax({
+//       url: 'http://gudetama.local:8000/api/v1/me',
+//       headers: {
+//         "Authorization": "Bearer " + accessToken
+//       },
+//       success: function(response) {
+//           vm.set('user', response.data);
+//           vm.set('isLoggedIn', true);
+//           resolve();
+//       },
+//       error: function() {
+//         $.ajax({
+//           url: 'http://gudetama.local:8000/oauth/token',
+//             method: 'POST',
+//             data: {
+//               grant_type: 'refresh_token',
+//               client_id: 2,
+//               client_secret: 'iMupCxM9eatJ963MUOoBp9uf5NAPfS3uZzebmp1I',
+//               refresh_token: refreshToken,
+//               scope: '*'
+//             },
+//             contentType: "application/x-www-form-urlencoded",
+//             headers: {
+//               'Accept': 'application/json'
+//             },
+//             success: function(response) {
+//               localStorage.setItem('access_token', response.access_token);
+//               localStorage.setItem('refresh_token', response.refresh_token);
+//               // application.advanceReadiness();
+//               resolve();
+//             },
+//             error: function() {
+//               reject();
+//             }
+//         });
 
-        session.preloadApplication();
-        application.advanceReadiness();
-      },
-      function() {
-        // Kill sesion object
-        session.set('user', '');
-        session.set('isLoggedIn', false);
-        application.advanceReadiness();
-      }
-    });
-   }
- } else {
-    application.advanceReadiness();
- }
-}
+//         session.preloadApplication();
+//         application.advanceReadiness();
+//       },
+//       function() {
+//         // Kill sesion object
+//         session.set('user', '');
+//         session.set('isLoggedIn', false);
+//         application.advanceReadiness();
+//       }
+//     });
+//    }
+//  } else {
+//     application.advanceReadiness();
+//  }
+// }
 
 // export default Ember.Application.initializer({
 //     name: 'loadUser',
@@ -132,51 +132,57 @@ export function initialize(container, application) {
 //         });
 //       }
 //     }
-  // loadCurrentUser() {
-  //   return new Promise((resolve, reject) => {
-  //       var accessToken = localStorage.getItem('access_token');
-  //       var refreshToken = localStorage.getItem('refresh_token');
-  //       var vm = this;
-  //       if (!!accessToken && accessToken.length > 0) {
-  //       $.ajax({
-  //           url: 'http://gudetama.local:8000/api/v1/me',
-  //           headers: {
-  //             "Authorization": "Bearer " + accessToken
-  //           },
-  //           success: function(response) {
-  //               vm.set('user', response.data);
-  //               vm.set('isLoggedIn', true);
-  //               resolve();
-  //           },
-  //           error: function() {
-  //             $.ajax({
-  //               url: 'http://gudetama.local:8000/oauth/token',
-  //                 method: 'POST',
-  //                 data: {
-  //                   grant_type: 'refresh_token',
-  //                   client_id: 2,
-  //                   client_secret: 'iMupCxM9eatJ963MUOoBp9uf5NAPfS3uZzebmp1I',
-  //                   refresh_token: refreshToken,
-  //                   scope: '*'
-  //                 },
-  //                 contentType: "application/x-www-form-urlencoded",
-  //                 headers: {
-  //                   'Accept': 'application/json'
-  //                 },
-  //                 success: function(response) {
-  //                   localStorage.setItem('access_token', response.access_token);
-  //                   localStorage.setItem('refresh_token', response.refresh_token);
-  //                   resolve();
-  //                 },
-  //                 error: function() {
-  //                   reject();
-  //                 }
-  //             });
-  //           }
-  //       });
-  //     } else {
-  //       reject();
-  //     }
-  //   });
-  // }
-// });
+  loadCurrentUser() {
+    return new Promise((resolve, reject) => {
+        var accessToken = localStorage.getItem('access_token');
+        var refreshToken = localStorage.getItem('refresh_token');
+        var vm = this;
+        if (!!accessToken && accessToken.length > 0) {
+        $.ajax({
+            url: 'http://gudetama.local:8000/api/v1/me',
+            headers: {
+              "Authorization": "Bearer " + accessToken
+            },
+            success: function(response) {
+                vm.set('user', response.data);
+                vm.set('isLoggedIn', true);
+                alert(response.data.name + " is logged in");
+                // console.log(response.data.name);
+                resolve();
+            },
+            error: function() {
+              $.ajax({
+                url: 'http://gudetama.local:8000/oauth/token',
+                  method: 'POST',
+                  data: {
+                    grant_type: 'refresh_token',
+                    client_id: 2,
+                    client_secret: 'iMupCxM9eatJ963MUOoBp9uf5NAPfS3uZzebmp1I',
+                    refresh_token: refreshToken,
+                    scope: '*'
+                  },
+                  contentType: "application/x-www-form-urlencoded",
+                  headers: {
+                    'Accept': 'application/json'
+                  },
+                  success: function(response) {
+                    localStorage.setItem('access_token', response.access_token);
+                    localStorage.setItem('refresh_token', response.refresh_token);
+                    resolve();
+                  },
+                  error: function() {
+                    alert("Invalid Login Credentials!");
+                    console.log("failed");
+                    reject();
+                  }
+              });
+              alert("Invalid Login Credentials!");
+              console.log("failed2");
+            }
+        });
+      } else {
+        reject();
+      }
+    });
+  }
+});
